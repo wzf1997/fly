2fd48197994e98684ec2d7675fcf849a8c06ce23
汪正飞 <wangzhengfei@shizhuang-inc.com>
2022-03-31 00:35:57 +0800

    ci: 增加脚本

---
+++ scripts/test.ts
@@ -0,0 +1,3 @@
+// import add from '@fly/util'
+
+// console.error(add(2, 3))
--- test.ts
+++
@@ -1,9 +0,0 @@
-export default function add(a: number, b: number) {
-  return a + b
-}
-export function add2(a: number, b: number) {
-  return a + b
-}
-export function add3(a: number, b: number) {
-  return a + b
-}
